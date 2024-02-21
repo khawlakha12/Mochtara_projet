58916f50edd6347264894c759c2d4259c6fc14fe
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-02-22 00:39:29 +0100

    front de mochtara

--- mochtara/resources/views/home.blade.php
+++ mochtara/resources/views/home.blade.php
@@ -176,7 +176,7 @@
   </h2>
   <section class="mb-8 mt-6">
     <div class="justify-center flex flex-wrap">
-      <div class="group my-12 flex w-full max-w-xs flex-col overflow-hidden border border-gray-100 bg-white shadow-md">
+      <div class="group my-12 flex w-full max-w-xs flex-col overflow-hidden border border-gray-100 bg-white shadow-md ml-2 mr-6 ">
         <a class="relative flex h-60 overflow-hidden" href="#">
           <img class="absolute top-0 right-0 h-full w-full object-cover" src="https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1072&q=80" alt="product image" />
           <div class="absolute bottom-0 mb-4 flex w-full justify-center space-x-4">
@@ -210,7 +210,7 @@
           </button>
         </div>
       </div>
-      <div class="group my-12 flex w-full max-w-xs flex-col overflow-hidden border border-gray-100 bg-white shadow-md">
+      <div class="group my-12 flex w-full max-w-xs flex-col overflow-hidden border border-gray-100 bg-white shadow-md ml-2 mr-6">
         <a class="relative flex h-60 overflow-hidden" href="#">
           <img class="absolute top-0 right-0 h-full w-full object-cover" src="https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1072&q=80" alt="product image" />
           <div class="absolute bottom-0 mb-4 flex w-full justify-center space-x-4">
@@ -244,7 +244,7 @@
           </button>
         </div>
       </div>
-      <div class="group my-12 flex w-full max-w-xs flex-col overflow-hidden border border-gray-100 bg-white shadow-md">
+      <div class="group my-12 flex w-full max-w-xs flex-col overflow-hidden border border-gray-100 bg-white shadow-md ml-2 mr-6">
         <a class="relative flex h-60 overflow-hidden" href="#">
           <img class="absolute top-0 right-0 h-full w-full object-cover" src="https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1072&q=80" alt="product image" />
           <div class="absolute bottom-0 mb-4 flex w-full justify-center space-x-4">
@@ -278,7 +278,7 @@
           </button>
         </div>
       </div>
-      <div class="group my-12 flex w-full max-w-xs flex-col overflow-hidden border border-gray-100 bg-white shadow-md">
+      <div class="group my-12 flex w-full max-w-xs flex-col overflow-hidden border border-gray-100 bg-white shadow-md ml-2 mr-6">
         <a class="relative flex h-60 overflow-hidden" href="#">
           <img class="absolute top-0 right-0 h-full w-full object-cover" src="https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1072&q=80" alt="product image" />
           <div class="absolute bottom-0 mb-4 flex w-full justify-center space-x-4">
