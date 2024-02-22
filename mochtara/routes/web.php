0c5967820aaceb66eb68b7d6e92bd9fea9deb269
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-02-22 12:01:53 +0100

    login

--- mochtara/routes/web.php
+++ mochtara/routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
-Route::get('/login',function(){
-    return view('pages.auth.signin');
+Route::get('/login', function() {
+    return view('pages.Auth.login');
 });
