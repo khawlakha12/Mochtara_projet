021fddcc12a629a56778abdb6635c9edd91ccbe3
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-02-22 12:09:05 +0100

    login front-end

--- mochtara/routes/web.php
+++ mochtara/routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('pages.home');
 });
-Route::get('/login', function() {
+Route::get('/', function() {
     return view('pages.Auth.login');
 });
