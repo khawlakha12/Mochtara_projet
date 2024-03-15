794cc319d1e9b0384de2aa5c1ef8d0310d262dce
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-03-15 12:22:05 +0000

    page shop front

--- mochtara/resources/views/pages/shop.blade.php
+++ mochtara/resources/views/pages/shop.blade.php
@@ -2,5 +2,18 @@
 
 <body>
 
-  <x-navbar />
-
+    <x-navbar />
+    <div class="flex justify-center mt-8">
+        <img class="rounded-lg" src="https://tecdn.b-cdn.net/img/new/slides/041.jpg" alt="">
+    </div>
+    <div class="flex  justify-center mt-8"> 
+      <div class="border rounded-lg bg-black h-24 w-80 flex items-center ">
+        <img src="https://tecdn.b-cdn.net/img/new/avatars/2.webp" class=" ml-6 border border-dashed rounded-full" style="width:20%; hieght:20%;" alt="">
+      </div>
+      <div class="border rounded-lg bg-black h-24 w-80 flex items-center mr-12 ml-12">
+        <img src="https://tecdn.b-cdn.net/img/new/avatars/2.webp" class=" ml-6 border border-dashed rounded-full" style="width:20%; hieght:20%;" alt="">
+      </div>
+      <div class="border rounded-lg bg-black h-24 w-80 flex items-center">
+        <img src="https://tecdn.b-cdn.net/img/new/avatars/2.webp" class=" ml-6 border border-dashed rounded-full" style="width:20%; hieght:20%;" alt="">
+      </div>
+  </div>
