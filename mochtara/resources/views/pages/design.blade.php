2054c93c4d9d2e96021232c9217de6381ef7d6fa
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-03-20 15:21:05 +0000

    page design

--- mochtara/resources/views/pages/design.blade.php
+++ mochtara/resources/views/pages/design.blade.php
@@ -4,6 +4,20 @@
 
     <x-navbar />
 
+    <article class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto mt-24">
+        <img src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a" alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
+        <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+        <h3 class="z-10 mt-3 text-3xl font-bold text-white">Paris</h3>
+        <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">City of love</div>
+    </article>
+
+    <article class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto mt-24">
+        <img src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a" alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
+        <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+        <h3 class="z-10 mt-3 text-3xl font-bold text-white">Paris</h3>
+        <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">City of love</div>
+    </article>
+
     <div class="flex justify-center  items-center h-screen gap-x-40 ">
         <div class="flex flex-col justify-center  items-center">
             <button class="bg-black text-white rounded-full w-10 h-10 flex items-center justify-center mt-4 "
