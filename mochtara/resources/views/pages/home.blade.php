fa9fdb87054e15dcd1da678ca040a5164c8b4e1c
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-03-21 22:41:04 +0000

    front de projet

--- mochtara/resources/views/pages/home.blade.php
+++ mochtara/resources/views/pages/home.blade.php
@@ -1,46 +1,48 @@
 <x-navbar />
 
 <main id="main" data-aos="fade" data-aos-delay="1500">
-  <!-- Page Header Removed for clarity -->
+    <!-- Page Header Removed for clarity -->
 
-  <section class="sample-page">
-      <div class="container" data-aos="fade-up">
-          <div class="row align-items-center">
-              <!-- Image on the Left -->
-              <div class="col-md-6">
-                  <img src="your-image-url.jpg" class="img-fluid" alt="Sample Image">
-              </div>
-
-              <!-- Text on the Right -->
-              <div class="col-md-6">
-                  <h2>Sample Inner Page</h2>
-                  <p>Odio et unde deleniti. Deserunt numquam exercitationem. Officiis quo odio sint voluptas consequatur ut a odio voluptatem. Sit dolorum debitis veritatis natus dolores. Quasi ratione sint. Sit quaerat ipsum dolorem.</p>
-                  <a class="cta-btn" href="contact.html">Available for hire</a>
-                  <p>You can duplicate this sample page and create any number of inner pages you like!</p>
-              </div>
-          </div>
-      </div>
-  </section>
+    <section class="sample-page">
+        <div class="container" data-aos="fade-up" style="margin-top: 100px;">
+            <div class="row align-items-center">
+                <!-- Image on the Left -->
+                <div class="col-md-6">
+                    <img src="img/home.png" class="img-fluid" alt="design">
+                </div>
+                <!-- Text on the Right -->
+                <div class="col-md-6">
+                    <h2>Sample Inner Page</h2>
+                    <p>Odio et unde deleniti. Deserunt numquam exercitationem. Officiis quo odio sint voluptas
+                        consequatur ut a odio voluptatem. Sit dolorum debitis veritatis natus dolores. Quasi ratione
+                        sint. Sit quaerat ipsum dolorem.</p>
+                    <a class="cta-btn" href="contact.html">Available for hire</a>
+                    <p>You can duplicate this sample page and create any number of inner pages you like!</p>
+                </div>
+            </div>
+        </div>
+    </section>
 </main><!-- End #main -->
 
-  <!-- ======= Footer ======= -->
-  <x-footer />
+<!-- ======= Footer ======= -->
+<x-footer />
 
-  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+<a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i
+        class="bi bi-arrow-up-short"></i></a>
 
-  <div id="preloader">
+<div id="preloader">
     <div class="line"></div>
-  </div>
+</div>
 
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+<!-- Vendor JS Files -->
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
 
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
 
 </body>
 
