3be1e6656d12c52b1992a2180ac83ca82bb45f62
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-03-22 01:49:26 +0000

    front projet

--- mochtara/resources/views/pages/home.blade.php
+++ mochtara/resources/views/pages/home.blade.php
@@ -1,19 +1,17 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css">
 <style>
-  .bilingual-btn {
-    
-    font-weight: bold; 
-    padding: 10px 20px; 
-    font-size: 16px; 
-    font-style: italic;
-    
-}
+    .bilingual-btn {
+
+        font-weight: bold;
+        padding: 10px 20px;
+        font-size: 16px;
+        font-style: italic;
+
+    }
 </style>
 <x-navbar />
 
 <main id="main" data-aos="fade" data-aos-delay="1500">
-    <!-- Page Header Removed for clarity -->
-
     <section class="sample-page">
         <div class="container" data-aos="fade-up" style="margin-top: 100px;">
             <div class="row align-items-center">
@@ -24,40 +22,35 @@
                 <!-- Text on the Right -->
                 <div class="col-md-6 text-center" style="margin-top: 40px;">
                     <h2><em>Welcome to Mochtara</em></h2>
-                    <p class="font-normal text-base leading-6 text-gray-600 dark:text-white">أول موقع مغربي يمكن من خلاله وضع أي
-                      تصميم لديك (صورة ,شعار ,رسمة أو مقولة…الخ) على تيشرت ذو جودة عالية وذالك عبر ستديو يتيح لك اختيار نوع و قياس
-                      و لون التيشرت</p>
+                    <p class="font-normal text-base leading-6 text-gray-600 dark:text-white">أول موقع مغربي يمكن من
+                        خلاله وضع أي
+                        تصميم لديك (صورة ,شعار ,رسمة أو مقولة…الخ) على تيشرت ذو جودة عالية وذالك عبر ستديو يتيح لك
+                        اختيار نوع و قياس
+                        و لون التيشرت</p>
                     <p>Design at Mochtara is simple. Start
-                      with a t-shirt/hoodie/sweatshirt/etc. from our broad inventory and quickly personalize itusing our intuitive
-                      Design Studio.Upload your artwork or your design,and design templates to make your own shirt in no time.</p>
-                      <a class="btn bilingual-btn" style="background: #27a776;" href="/design" role="button"> اطبع الان Print Now </a>
-                      
+                        with a t-shirt/hoodie/sweatshirt/etc. from our broad inventory and quickly personalize itusing
+                        our intuitive
+                        Design Studio.Upload your artwork or your design,and design templates to make your own shirt in
+                        no time.</p>
+                    <a class="btn bilingual-btn" style="background: #27a776;" href="/design" role="button"> اطبع الان
+                        Print Now </a>
+
                 </div>
             </div>
         </div>
     </section>
-</main><!-- End #main -->
+</main>
+<!-- End #main -->
 
 <!-- ======= Footer ======= -->
 <x-footer />
-
 <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i
         class="bi bi-arrow-up-short"></i></a>
 
 <div id="preloader">
     <div class="line"></div>
 </div>
-
-<!-- Vendor JS Files -->
-<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-<script src="assets/vendor/aos/aos.js"></script>
-<script src="assets/vendor/php-email-form/validate.js"></script>
-
-<!-- Template Main JS File -->
-<script src="assets/js/main.js"></script>
-
+<x-link_script />
 </body>
 
 </html>
