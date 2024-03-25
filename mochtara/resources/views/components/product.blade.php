b1b680c5e3cf2323b82548e0b0823e69a3e3d215
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-03-25 11:30:21 +0000

    product

--- mochtara/resources/views/components/product.blade.php
+++ mochtara/resources/views/components/product.blade.php
@@ -9,18 +9,18 @@
 </head>
 <body>
     <section id="Projects"
-    class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
-    <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+    class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 justify-items-center justify-center gap-x-14 mt-10 mb-5">
+    <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl" style="height:75%;">
         <a href="#">
-            <img src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                    alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
+            <img src="img\9obya.png" style="height:65%;"
+                    alt="Product" class="h-69 w-72 object-cover rounded-t-xl" />
             <div class="px-4 py-3 w-72">
                 <span class="text-gray-400 mr-3 uppercase text-xs">Brand</span>
                 <p class="text-lg font-bold text-black truncate block capitalize">Product Name</p>
                 <div class="flex items-center">
-                    <p class="text-lg font-semibold text-black cursor-auto my-3">$149</p>
+                    <p class="text-lg font-semibold text-black cursor-auto my-3">MAD 149</p>
                     <del>
-                        <p class="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+                        <p class="text-sm text-gray-600 cursor-auto ml-2">MAD 199</p>
                     </del>
                     <div class="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                             fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
@@ -33,20 +33,18 @@
             </div>
         </a>
     </div>
-    <!--   🛑 Product card 1 - Ends Here  -->
 
-    <!--   ✅ Product card 2 - Starts Here 👇 -->
-    <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+    <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl" style="height:75%;">
         <a href="#">
-            <img src="https://images.unsplash.com/photo-1651950519238-15835722f8bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mjh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                    alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
+            <img src="img\9obya.png" style="height:65%;"
+                    alt="Product" class="h-69 w-72 object-cover rounded-t-xl" />
             <div class="px-4 py-3 w-72">
                 <span class="text-gray-400 mr-3 uppercase text-xs">Brand</span>
                 <p class="text-lg font-bold text-black truncate block capitalize">Product Name</p>
                 <div class="flex items-center">
-                    <p class="text-lg font-semibold text-black cursor-auto my-3">$149</p>
+                    <p class="text-lg font-semibold text-black cursor-auto my-3">MAD 149</p>
                     <del>
-                        <p class="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+                        <p class="text-sm text-gray-600 cursor-auto ml-2">MAD 199</p>
                     </del>
                     <div class="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                             fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
@@ -59,20 +57,18 @@
             </div>
         </a>
     </div>
-    <!--   🛑 Product card 2- Ends Here  -->
 
-    <!--   ✅ Product card 3 - Starts Here 👇 -->
-    <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+    <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl" style="height:75%;">
         <a href="#">
-            <img src="https://images.unsplash.com/photo-1651950537598-373e4358d320?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MjV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                    alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
+            <img src="img\9obya.png" style="height:65%;"
+                    alt="Product" class="h-69 w-72 object-cover rounded-t-xl" />
             <div class="px-4 py-3 w-72">
                 <span class="text-gray-400 mr-3 uppercase text-xs">Brand</span>
                 <p class="text-lg font-bold text-black truncate block capitalize">Product Name</p>
                 <div class="flex items-center">
-                    <p class="text-lg font-semibold text-black cursor-auto my-3">$149</p>
+                    <p class="text-lg font-semibold text-black cursor-auto my-3">MAD 149</p>
                     <del>
-                        <p class="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+                        <p class="text-sm text-gray-600 cursor-auto ml-2">MAD 199</p>
                     </del>
                     <div class="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                             fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
@@ -85,20 +81,18 @@
             </div>
         </a>
     </div>
-    <!--   🛑 Product card 3 - Ends Here  -->
 
-    <!--   ✅ Product card 4 - Starts Here 👇 -->
-    <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+    <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl" style="height:75%;">
         <a href="#">
-            <img src="https://images.unsplash.com/photo-1651950540805-b7c71869e689?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mjl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                    alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
+            <img src="img\9obya.png" style="height:65%;"
+                    alt="Product" class="h-69 w-72 object-cover rounded-t-xl" />
             <div class="px-4 py-3 w-72">
                 <span class="text-gray-400 mr-3 uppercase text-xs">Brand</span>
                 <p class="text-lg font-bold text-black truncate block capitalize">Product Name</p>
                 <div class="flex items-center">
-                    <p class="text-lg font-semibold text-black cursor-auto my-3">$149</p>
+                    <p class="text-lg font-semibold text-black cursor-auto my-3">MAD 149</p>
                     <del>
-                        <p class="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+                        <p class="text-sm text-gray-600 cursor-auto ml-2">MAD 199</p>
                     </del>
                     <div class="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                             fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
@@ -111,20 +105,18 @@
             </div>
         </a>
     </div>
-    <!--   🛑 Product card 4 - Ends Here  -->
 
-    <!--   ✅ Product card 5 - Starts Here 👇 -->
-    <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+    <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl" style="height:75%;">
         <a href="#">
-            <img src="https://images.unsplash.com/photo-1649261191624-ca9f79ca3fc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                    alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
+            <img src="img\9obya.png" style="height:65%;"
+                    alt="Product" class="h-69 w-72 object-cover rounded-t-xl" />
             <div class="px-4 py-3 w-72">
                 <span class="text-gray-400 mr-3 uppercase text-xs">Brand</span>
                 <p class="text-lg font-bold text-black truncate block capitalize">Product Name</p>
                 <div class="flex items-center">
-                    <p class="text-lg font-semibold text-black cursor-auto my-3">$149</p>
+                    <p class="text-lg font-semibold text-black cursor-auto my-3">MAD 149</p>
                     <del>
-                        <p class="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+                        <p class="text-sm text-gray-600 cursor-auto ml-2">MAD 199</p>
                     </del>
                     <div class="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                             fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
@@ -137,20 +129,18 @@
             </div>
         </a>
     </div>
-    <!--   🛑 Product card 5 - Ends Here  -->
 
-    <!--   ✅ Product card 6 - Starts Here 👇 -->
-    <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+    <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl" style="height:75%;">
         <a href="#">
-            <img src="https://images.unsplash.com/photo-1649261191606-cb2496e97eee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                    alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
+            <img src="img\9obya.png" style="height:65%;"
+                    alt="Product" class="h-69 w-72 object-cover rounded-t-xl" />
             <div class="px-4 py-3 w-72">
                 <span class="text-gray-400 mr-3 uppercase text-xs">Brand</span>
                 <p class="text-lg font-bold text-black truncate block capitalize">Product Name</p>
                 <div class="flex items-center">
-                    <p class="text-lg font-semibold text-black cursor-auto my-3">$149</p>
+                    <p class="text-lg font-semibold text-black cursor-auto my-3">MAD 149</p>
                     <del>
-                        <p class="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+                        <p class="text-sm text-gray-600 cursor-auto ml-2">MAD 199</p>
                     </del>
                     <div class="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                             fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
@@ -163,7 +153,7 @@
             </div>
         </a>
     </div>
-    <!--   🛑 Product card 6 - Ends Here  -->
+
 
 </section>
 </body>
