0757b62367b8f7c4b3c06379e8369d42ff33d0b8
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-03-25 11:47:41 +0000

    front page design

--- mochtara/resources/views/pages/design.blade.php
+++ mochtara/resources/views/pages/design.blade.php
@@ -1,200 +1,37 @@
-<x-link_home />
+<x-navbar />
 
-<body>
-    <x-navbar />
+    <main id="main" data-aos="fade" data-aos-delay="1500">
 
-<section class="">
-    <div class="flex justify-center items-center h-screen gap-x-40 ml-10">
-        <div id="scroll-container" style="max-height:700px; overflow:hidden">
-            <article
-                class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto w-48 h-48 mt-4">
-                <img src="img/hoodies-catégorie.webp"
-                    alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
-                <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-                <h3 class="z-10 mt-3 text-xl font-bold text-white">hoodies</h3>
-                <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">City of love</div>
-            </article>
-            <article
-                class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto w-48 h-48 mt-4">
-                <img src="img/sweatshirt.webp"
-                    alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
-                <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-                <h3 class="z-10 mt-3 text-xl font-bold text-white">Sweatshirts</h3>
-                <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">City of love</div>
-            </article>
-
-            <article
-                class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto w-48 h-48  mt-4">
-                <img src="img/tshirt.webp"
-                    alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
-                <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-                <h3 class="z-10 mt-3 text-xl font-bold text-white">t-shirts</h3>
-                <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">City of love</div>
-            </article>
-
-            <article
-                class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto w-48 h-48  mt-4">
-                <img src="img/pillow.webp"
-                    alt="University of Southern California" class="absolute inset-0 h-full w-full object-cover">
-                <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-                <h3 class="z-10 mt-3 text-xl font-bold text-white">Pillows</h3>
-                <div class="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">City of love</div>
-            </article>
-        </div>
-
-        <div class="flex flex-col items-center justify-center gap-y-2" style="height: 70%; width: 40%;">
-            <div class="flex gap-x-6 ">
-                <div class="border-2 rounded-full h-7 w-7 bg-yellow-400 border-indigo-600"></div>
-                <div class="border-2 rounded-full h-7 w-7 bg-red-400 border-indigo-600"></div>
-                <div class="border-2 rounded-full h-7 w-7 bg-fuchsia-500 border-indigo-600"></div>
-                <div class="border-2 rounded-full h-7 w-7 bg-indigo-600 border-indigo-600"></div>
+        <!-- ======= End Page Header ======= -->
+        <div class="page-header d-flex align-items-center">
+          <div class="container position-relative">
+            <div class="row d-flex justify-content-center">
+              <div class="col-lg-6 text-center">
+                <h2>Sample Inner Page</h2>
+                <p>Odio et unde deleniti. Deserunt numquam exercitationem. Officiis quo odio sint voluptas consequatur ut a odio voluptatem. Sit dolorum debitis veritatis natus dolores. Quasi ratione sint. Sit quaerat ipsum dolorem.</p>
+    
+                <a class="cta-btn" href="contact.html">Available for hire</a>
+    
+              </div>
             </div>
-            <div
-                class=" flex justify-center items-center rounded-lg relative">
-                <div class="flex flex-col justify-center items-center ">
-                    <img src="img/div design-t-shirt.png" class="rounded-xl" style="height: 400px; width : 600px;" alt="">
-                    <div class="absolute bottom-0 right-0 mb-4 mr-4">
-                        <button class="bg-white text-white rounded-full w-10 h-10 flex items-center justify-center">
-                            <svg fill="#000000" height="65%" width="65%" version="1.1" id="Capa_1"
-                                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                viewBox="0 0 452.025 452.025" xml:space="preserve">
-                                <g>
-                                    <g>
-                                        <path d="M147.912,363.325c-4.7-4.7-12.3-4.7-17,0c-4.7,4.7-4.7,12.3,0,17l13.6,13.6h-55.2c-35.9,0-65-29.2-65-65v-40.3
-                                   c0-6.6-5.4-12-12-12s-12,5.4-12,12v40.3c0,49.1,39.9,89,89,89h55.2l-13.6,13.6c-4.7,4.7-4.7,12.3,0,17c2.3,2.3,5.4,3.5,8.5,3.5
-                                   s6.1-1.2,8.5-3.5l34.1-34.1c4.7-4.7,4.7-12.3,0-17L147.912,363.325z" />
-                                        <path
-                                            d="M210.312,0.025h-197.1c-6.6,0-12,5.4-12,12v197.1c0,6.6,5.4,12,12,12h197.1c6.6,0,12-5.4,12-12v-197.1
-                                   C222.312,5.425,217.012,0.025,210.312,0.025z M198.312,197.125h-173.1v-173.1h173.1V197.125z" />
-                                        <path d="M362.612,34.125h-55.2l13.6-13.6c4.7-4.7,4.7-12.3,0-17s-12.3-4.7-17,0l-34,34.1c-4.7,4.7-4.7,12.3,0,17l34.1,34.1
-                                   c2.3,2.3,5.4,3.5,8.5,3.5s6.1-1.2,8.5-3.5c4.7-4.7,4.7-12.3,0-17l-13.6-13.6h55.2c35.9,0,65,29.2,65,65v40.3c0,6.6,5.4,12,12,12
-                                   s12-5.4,12-12v-40.3C451.712,74.025,411.712,34.125,362.612,34.125z" />
-                                        <path
-                                            d="M438.812,428.025c-3.2,0-6.3,1.3-8.5,3.5s-3.5,5.3-3.5,8.5c0,3.1,1.3,6.3,3.5,8.5s5.3,3.5,8.5,3.5s6.3-1.3,8.5-3.5
-                                   s3.5-5.3,3.5-8.5s-1.3-6.3-3.5-8.5C445.012,429.325,441.912,428.025,438.812,428.025z" />
-                                        <path
-                                            d="M389.512,428.025c-6.6,0-12,5.4-12,12s5.4,12,12,12s12-5.4,12-12S396.112,428.025,389.512,428.025z" />
-                                        <path
-                                            d="M290.912,428.025c-6.6,0-12,5.4-12,12s5.4,12,12,12s12-5.4,12-12S297.612,428.025,290.912,428.025z" />
-                                        <path
-                                            d="M340.212,428.025c-6.6,0-12,5.4-12,12s5.4,12,12,12s12-5.4,12-12S346.812,428.025,340.212,428.025z" />
-                                        <path d="M241.713,428.025c-3.2,0-6.3,1.3-8.5,3.5s-3.5,5.3-3.5,8.5c0,3.1,1.3,6.3,3.5,8.5s5.3,3.5,8.5,3.5c3.1,0,6.3-1.3,8.5-3.5
-                                   s3.5-5.3,3.5-8.5s-1.3-6.3-3.5-8.5S244.812,428.025,241.713,428.025z" />
-                                        <path
-                                            d="M241.713,378.725c-6.6,0-12,5.4-12,12s5.4,12,12,12s12-5.4,12-12S248.312,378.725,241.713,378.725z" />
-                                        <path
-                                            d="M241.713,329.425c-6.6,0-12,5.4-12,12s5.4,12,12,12s12-5.4,12-12S248.312,329.425,241.713,329.425z" />
-                                        <path
-                                            d="M241.713,280.125c-6.6,0-12,5.4-12,12s5.4,12,12,12s12-5.4,12-12S248.312,280.125,241.713,280.125z" />
-                                        <path
-                                            d="M241.713,230.925c-3.2,0-6.3,1.3-8.5,3.5s-3.5,5.3-3.5,8.5s1.3,6.3,3.5,8.5s5.3,3.5,8.5,3.5c3.1,0,6.3-1.3,8.5-3.5
-                                   s3.5-5.3,3.5-8.5c0-3.1-1.3-6.3-3.5-8.5C247.912,232.125,244.812,230.925,241.713,230.925z" />
-                                        <path
-                                            d="M389.512,230.925c-6.6,0-12,5.4-12,12s5.4,12,12,12s12-5.4,12-12S396.112,230.925,389.512,230.925z" />
-                                        <path
-                                            d="M340.212,230.925c-6.6,0-12,5.4-12,12s5.4,12,12,12s12-5.4,12-12S346.812,230.925,340.212,230.925z" />
-                                        <path
-                                            d="M290.912,230.925c-6.6,0-12,5.4-12,12s5.4,12,12,12s12-5.4,12-12S297.612,230.925,290.912,230.925z" />
-                                        <path
-                                            d="M438.812,230.925c-3.2,0-6.3,1.3-8.5,3.5s-3.5,5.3-3.5,8.5s1.3,6.3,3.5,8.5s5.3,3.5,8.5,3.5s6.3-1.3,8.5-3.5
-                                   s3.5-5.3,3.5-8.5c0-3.1-1.3-6.3-3.5-8.5C445.012,232.225,441.912,230.925,438.812,230.925z" />
-                                        <path
-                                            d="M438.812,280.125c-6.6,0-12,5.4-12,12s5.4,12,12,12s12-5.4,12-12S445.412,280.125,438.812,280.125z" />
-                                        <path
-                                            d="M438.812,378.725c-6.6,0-12,5.4-12,12s5.4,12,12,12s12-5.4,12-12S445.412,378.725,438.812,378.725z" />
-                                        <path
-                                            d="M438.812,329.425c-6.6,0-12,5.4-12,12s5.4,12,12,12s12-5.4,12-12S445.412,329.425,438.812,329.425z" />
-                                    </g>
-                                </g>
-                            </svg>
-                        </button>
-                    </div>
+          </div>
+        </div><!-- End Page Header -->
+    
+        <section class="sample-page">
+          <div class="container" data-aos="fade-up">
+    
+            <p>
+              You can duplicate this sample page and create any number of inner pages you like!
+            </p>
+    
+          </div>
+        </section>
+    
+      </main><!-- End #main -->
 
-                </div>
+      <x-link_script />
 
-            </div>
-            <div class="flex justify-center  items-center gap-x-4 ">
-                <button class="bg-black text-white rounded-full w-10 h-10 flex items-center justify-center "
-                    onclick="backToTop()">
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M5 10l7-7m0 0l7 7m-7-7v18">
-                        </path>
-                    </svg>
-                </button>
-                <img src="img/design.webp" alt="" class="border-2 border-black h-24 w-24 mb-4  rounded-lg">
-                <img src="img/design.webp" alt="" class="border-2 border-black h-24 w-24 mb-4 rounded-lg">
-                <img src="img/design.webp" alt="" class="border-2 border-black h-24 w-24 mb-4 rounded-lg">
-                <img src="img/design.webp" alt="" class="border-2 border-black h-24 w-24 mb-4 rounded-lg">
-                <button class="bg-black text-white rounded-full w-10 h-10 flex items-center justify-center"
-                    onclick="backToTop()">
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M5 10l7-7m0 0l7 7m-7-7v18">
-                        </path>
-                    </svg>
-                </button>
-            </div>
-            <a class="relative mt-8" href="#">
-                <span class="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-black"></span>
-                <span
-                    class="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-yellow-400 hover:text-gray-900 dark:bg-transparent">Print</span>
-            </a>
-        </div>
-        <div class="flex flex-col gap-y-4" >
-           
-            <a href="javascript:;"
-                class="relative bg-cover group rounded-2xl bg-center overflow-hidden mx-auto sm:ml-0 xl:mx-auto h-69 w-48 cursor-pointer">
-                <img src="img/t-shirt.webp" alt="Denim jacket image">
-                <div
-                    class="absolute bottom-3 left-0 mx-3  bg-white w-[calc(100%-24px)] rounded-xl shadow-sm shadow-transparent transition-all duration-500 group-hover:shadow-indigo-200 group-hover:bg-indigo-50">
-                    <div class="flex items-center justify-between mb-2 mt-2">
-                        <p class="font-semibold px-1  text-indigo-600 text-right">MAD 100</p>
-                    </div>
-                </div>
-            </a>
-            <a href="javascript:;"
-                class="relative bg-cover group rounded-2xl bg-center overflow-hidden mx-auto sm:ml-0 xl:mx-auto h-69 w-48 cursor-pointer">
-                <img src="img/t-shirt.webp" alt="Denim jacket image">
-                <div
-                    class="absolute bottom-3 left-0 mx-3  bg-white w-[calc(100%-24px)] rounded-xl shadow-sm shadow-transparent transition-all duration-500 group-hover:shadow-indigo-200 group-hover:bg-indigo-50">
-                    <div class="flex items-center justify-between mb-2 mt-2">
-                        <p class="font-semibold px-1  text-indigo-600 text-right">MAD 100</p>
-                    </div>
-                </div>
-            </a>
-            <a href="javascript:;"
-                class="relative bg-cover group rounded-2xl bg-center overflow-hidden mx-auto sm:ml-0 xl:mx-auto h-69 w-48 cursor-pointer">
-                <img src="img/t-shirt.webp" alt="Denim jacket image">
-                <div
-                    class="absolute bottom-3 left-0 mx-3  bg-white w-[calc(100%-24px)] rounded-xl shadow-sm shadow-transparent transition-all duration-500 group-hover:shadow-indigo-200 group-hover:bg-indigo-50">
-                    <div class="flex items-center justify-between mb-2 mt-2">
-                        <p class="font-semibold px-1  text-indigo-600 text-right">MAD 100</p>
-                    </div>
-                </div>
-            </a>
-            
-        </div>
-    </div>
-</section>
-    <script>
-        function autoScroll() {
-            const container = document.getElementById('scroll-container');
+</body>
 
-            const scrollStep = 1;
-            const scrollInterval = 30;
-            const scroll = () => {
-                if (container.scrollTop < container.scrollHeight - container.clientHeight) {
-                    container.scrollBy(0, scrollStep);
-                } else {
+</html>
 
-                    container.scrollTop = 0;
-                }
-            };
-
-            setInterval(scroll, scrollInterval);
-        }
-        autoScroll();
-    </script>
