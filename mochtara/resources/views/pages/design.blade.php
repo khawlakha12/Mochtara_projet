8190479eaadb99d33e6a116be49b0646fe822c45
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-03-30 18:55:27 +0000

    front de projet

--- mochtara/resources/views/pages/design.blade.php
+++ mochtara/resources/views/pages/design.blade.php
@@ -6,11 +6,14 @@
     <div class="page-header d-flex align-items-center">
         <div class="container position-relative">
             <div class="row justify-content-center">
-                <div class="col-md-6 d-flex justify-content-center align-items-center">
-                  <div class="card-body bg-white rounded" style="height: 80%;">
-                    <div class="d-flex justify-content-center" style="height: 100%;">
-                        <h1 class="text-black"> add text</h1>
+                <div class="col-md-5 d-flex justify-content-center align-items-center">
+                  <div class="card-body bg-white rounded" style="height: 70%;">
+                    <div class="d-flex justify-content-center  align-items-center" style="height: 100%;">
+                        <h1 class="text-black rounded" style="border:2px dashed black;padding : 10px 40px;height:50%;"> add text</h1>
                     </div>
+                    <button type="button" class="btn btn-primary" style=" bottom: 0; left: 0; border-radius: 50%; width: 30px; height: 30px;">
+                      <i class="fas fa-plus"></i> 
+                  </button>
                 </div>
                 </div>
                 <div class="col-md-6">
