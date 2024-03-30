ef587fffa4197c807864b42369f887e1c06c7f41
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-03-30 18:07:33 +0000

    front page design

--- mochtara/resources/views/pages/design.blade.php
+++ mochtara/resources/views/pages/design.blade.php
@@ -1,36 +1,32 @@
 <x-navbar />
 
-    <main id="main" data-aos="fade" data-aos-delay="1500">
+<main id="main" data-aos="fade" data-aos-delay="1500">
 
-        <!-- ======= End Page Header ======= -->
-        <div class="page-header d-flex align-items-center">
-          <div class="container position-relative">
+    <!-- ======= End Page Header ======= -->
+    <div class="page-header d-flex align-items-center">
+        <div class="container position-relative">
             <div class="row justify-content-center">
-              <div class="col-md-4 d-flex justify-content-center">
-                <div class="card">
-                    <div class="card-body">
-                        <form>
-                            <div class="mb-3" style="height: 60%; width:60%;">
-                                <input class="form-control" type="file" id="image-upload" accept="image/*">
-                            </div>
-                        </form>
+                <div class="col-md-6 d-flex justify-content-center align-items-center">
+                  <div class="card-body bg-white rounded" style="height: 80%;">
+                    <div class="d-flex justify-content-center" style="height: 100%;">
+                        <h1 class="text-black"> add text</h1>
                     </div>
                 </div>
+                </div>
+                <div class="col-md-6">
+                    <img src="https://raw.githubusercontent.com/mobalti/open-props-interfaces/main/product-details/images/img-1.webp"
+                        alt="Description de la grande image" class="img-fluid">
+                </div>
             </div>
-            <div class="col-md-6">
-                <img  src="https://raw.githubusercontent.com/mobalti/open-props-interfaces/main/product-details/images/img-1.webp" alt="Description de la grande image" class="img-fluid">
-            </div>
-            </div>
-          </div>
         </div>
-    
-      
-    
-      </main>
+    </div>
 
-      <x-link_script />
+
+
+</main>
+
+<x-link_script />
 
 </body>
 
 </html>
-
