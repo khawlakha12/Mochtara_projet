83e204c45f7eb5c936d97bf3ae5404cf3749dff8
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-04-01 11:55:22 +0000

    front dash

--- mochtara/resources/views/pages/Admin/Categorie.blade.php
+++ mochtara/resources/views/pages/Admin/Categorie.blade.php
@@ -155,7 +155,23 @@
                                             </button>
                                         </div>
                                         <div class="modal-body">
-                                            
+                                            <div>
+                                                <div class="mb-4 d-flex justify-content-center">
+                                                    <img id="selectedImage" src="https://mdbootstrap.com/img/Photos/Others/placeholder.jpg"
+                                                    alt="example placeholder" style="width: 300px;" />
+                                                </div>
+                                                <div class="d-flex justify-content-center">
+                                                    <div class="btn btn-primary btn-rounded">
+                                                        <label class="form-label text-white m-1" for="customFile1">Choose file</label>
+                                                        <input type="file" class="form-control d-none" id="customFile1" onchange="displaySelectedImage(event, 'selectedImage')" />
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleInputEmail1" style="color:black;">Name</label>
+                                                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Name">
+                                                  </div>
+                                                
+                                            </div>
                                         </div>
                                         <div class="modal-footer">
                                             <button type="button" class="btn btn-secondary"
