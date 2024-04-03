51e08b85bcf978cd1a137834b3b87c903dcf432a
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-04-03 01:40:11 +0000

    barre de recherche

--- mochtara/resources/views/pages/shop.blade.php
+++ mochtara/resources/views/pages/shop.blade.php
@@ -54,7 +54,63 @@
         padding: 5px 10px;
     }
 </style>
-
+<style> .search-box{
+    width: fit-content;
+    height: fit-content;
+    position: relative;
+  }
+  .input-search{
+    height: 50px;
+    width: 50px;
+    border-style: none;
+    padding: 10px;
+    font-size: 18px;
+    letter-spacing: 2px;
+    outline: none;
+    border-radius: 25px;
+    transition: all .5s ease-in-out;
+    background-color: #27a776;
+    padding-right: 40px;
+    color:#fff;
+  }
+  .input-search::placeholder{
+    color:rgba(255,255,255,.5);
+    font-size: 18px;
+    letter-spacing: 2px;
+    font-weight: 100;
+  }
+  .btn-search{
+    width: 50px;
+    height: 50px;
+    border-style: none;
+    font-size: 20px;
+    font-weight: bold;
+    outline: none;
+    cursor: pointer;
+    border-radius: 50%;
+    position: absolute;
+    right: 0px;
+    color:#ffffff ;
+    background-color:transparent;
+    pointer-events: painted;  
+  }
+  .btn-search:focus ~ .input-search{
+    width: 300px;
+    border-radius: 0px;
+    background-color: transparent;
+    border-bottom:1px solid rgba(255,255,255,.5);
+    transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
+  }
+  .input-search:focus{
+    width: 300px;
+    border-radius: 0px;
+    background-color: transparent;
+    border-bottom:1px solid rgba(255,255,255,.5);
+    transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
+  }
+  </style>
+<link rel="stylesheet"href="style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <x-navbar />
 
 <!-- ======= Hero Section ======= -->
@@ -101,11 +157,11 @@
             </div>
     </section>
     <!-- ======= Search Section ======= -->
-    <section class="mt-4">
-        <div class="search d-flex justify-content-center align-items-center">
-            <input type="search" class="form-control" placeholder="search" />
-            <i class="bi bi-search search-icon"></i>
-        </div>
+    <section class="mt-4 flex justify-center ">
+        <div class="search-box">
+            <button class="btn-search"><i class="fas fa-search"></i></button>
+            <input type="text" class="input-search" placeholder="Type to Search...">
+          </div>
     </section>
     <!-- ======= Gallery Section ======= -->
 <x-product />
