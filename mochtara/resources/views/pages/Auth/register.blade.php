410c41d197c2a0b60d9eb418fc8ee540740fda9d
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-04-05 10:08:44 +0000

    authentification

--- mochtara/resources/views/pages/Auth/register.blade.php
+++ mochtara/resources/views/pages/Auth/register.blade.php
@@ -4,11 +4,7 @@
 		<meta charset="utf-8">
 		<title>Mochtara</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		
-		<!-- MATERIAL DESIGN ICONIC FONT -->
 		<link rel="stylesheet" href="Auth/fonts/material-design-iconic-font/css/material-design-iconic-font.min.css">
-
-		<!-- STYLE CSS -->
 		<link rel="stylesheet" href="Auth/css/style.css">
 	</head>
 
@@ -19,18 +15,18 @@
 				<div class="image-holder">
 					<img src="Auth/images/hoodies.webp" alt="">
 				</div>
-				<form action="">
+				<form action="" method="POST">
 					<h3>Register</h3>
 					<div class="form-group">
-						<input type="text" placeholder="First Name" class="form-control">
-						<input type="text" placeholder="Last Name" class="form-control">
+						<input type="text" name ="name" placeholder="First Name" class="form-control">
+						<input type="text" name ="name" placeholder="Last Name" class="form-control">
 					</div>
 					<div class="form-wrapper">
-						<input type="text" placeholder="Email Address" class="form-control">
+						<input type="email" name="email" placeholder="Email Address" class="form-control">
 						<i class="zmdi zmdi-email"></i>
 					</div>
 					<div class="form-wrapper">
-						<select name="" id="" class="form-control">
+						<select name="gender" id="" class="form-control">
 							<option value="" disabled selected>Gender</option>
 							<option value="male">Male</option>
 							<option value="femal">Female</option>
@@ -39,11 +35,11 @@
 						<i class="zmdi zmdi-caret-down" style="font-size: 17px"></i>
 					</div>
 					<div class="form-wrapper">
-						<input type="password" placeholder="Password" class="form-control">
+						<input type="password" name="password"placeholder="Password" class="form-control">
 						<i class="zmdi zmdi-lock"></i>
 					</div>
                     <span>I have <a href="">Account</a></span>
-					<button>Register
+					<button type="submit">Register
 						<i class="zmdi zmdi-arrow-right"></i>
 					</button>
 				</form>
