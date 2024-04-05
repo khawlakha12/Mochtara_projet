51756055e8714ee8a889c798363108f0c5b38688
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-04-05 15:52:13 +0000

    forgot password

--- mochtara/app/Http/Controllers/Auth/ForgotPasswordController.php
+++ mochtara/app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 
 class ForgotPasswordController extends Controller
 {
-    //
+    use SendsPasswordResetEmails;
 }
---
+++ mochtara/resources/views/pages/Auth/reset.blade.php
@@ -0,0 +1,9 @@
+<!-- /resources/views/auth/passwords/reset.blade.php -->
+<form method="POST" action="{{ route('password.update') }}">
+    @csrf
+    <input type="hidden" name="token" value="{{ $token }}">
+    <input type="email" name="email" required>
+    <input type="password" name="password" required>
+    <input type="password" name="password_confirmation" required>
+    <button type="submit">Réinitialiser le mot de passe</button>
+</form>
