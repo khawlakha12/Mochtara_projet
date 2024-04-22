72c2f77923e761940a6e01ec7d9097d5beec6bfe
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-04-22 14:47:43 +0100

    payment

--- mochtara/resources/views/components/payment.blade.php
+++ mochtara/resources/views/components/payment.blade.php
@@ -1,86 +1,188 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <style>
-@import url('https://fonts.googleapis.com/css?family=Work+Sans');
-
-.card1{
-  background:#16181a;  border-radius:14px; max-width: 300px; display:block; margin:auto;
-  padding:60px; padding-left:20px; padding-right:20px;box-shadow: 2px 10px 40px black; z-index:99;
+.form {
+  background: #000000;
+  border: 1px solid white;
+  backdrop-filter: 100px; 
+  animation: kfr-rotate45 2s! infinite;
+  box-shadow: 0px 187px 75px rgba(0, 0, 0, 0.01),
+    0px 105px 63px rgba(0, 0, 0, 0.05), 0px 47px 47px rgba(0, 0, 0, 0.09),
+    0px 12px 26px rgba(0, 0, 0, 0.1), 0px 0px 0px rgba(0, 0, 0, 0.1);
+  width: 320px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  padding: 20px;
+  position: relative;
+  border-radius: 25px;
+}
+.form .label {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  height: -moz-fit-content;
+  height: fit-content;
+}
+.form .label:has(input:focus) .title {
+  top: 0;
+  left: 0;
+  color: #27a776;
+}
+.form .label .title {
+  padding: 0 10px;
+  transition: all 300ms;
+  font-size: 12px;
+  color: #8b8e98;
+  font-weight: 600;
+  width: -moz-fit-content;
+  width: fit-content;
+  top: 14px;
+  position: relative;
+  left: 15px;
+  background: #0c0f14;
+}
+.form .input-field {
+  width: auto;
+  height: 50px;
+  text-indent: 15px;
+  border-radius: 15px;
+  outline: none;
+  background-color: transparent;
+  border: 1px solid #21262e;
+  transition: all 0.3s;
+  caret-color: #27a776;
+  color: #aeaeae;
 }
 
-.logo-card{max-width:50px; margin-bottom:15px; margin-top: -19px;}
-
-label{display:flex; font-size:10px; color:white; opacity:.4;}
-
-input{font-family: 'Work Sans', sans-serif;background:transparent; border:none; border-bottom:1px solid transparent; color:#dbdce0; transition: border-bottom .4s;}
-input:focus{border-bottom:1px solid #1abc9c; outline:none;}
-
-.cardnumber{display:block; font-size:20px; margin-bottom:8px; }
-
-.name{display:block; font-size:15px; max-width: 200px; float:left; margin-bottom:15px;}
-
-.toleft{float:left;}
-.ccv{width:50px; margin-top:-5px; font-size:15px;}
-
-.receipt{background: #dbdce0; border-radius:4px; padding:5%; padding-top:200px; max-width:600px; display:block; margin:auto; margin-top:-180px; z-index:-999; position:relative;}
-
-.col{width:50%; float:left;}
-.bought-item{background:#f5f5f5; padding:2px;}
-.bought-items{margin-top:-3px;}
-
-.cost{color:#3a7bd5;}
-.seller{color: #3a7bd5;}
-.description{font-size: 13px;}
-.price{font-size:12px;}
-.comprobe{text-align:center;}
-.proceed{position:absolute; transform:translate(300px, 10px); width:50px; height:50px; border-radius:50%; background:#1abc9c; border:none;color:white; transition: box-shadow .2s, transform .4s; cursor:pointer;}
-.proceed:active{outline:none; }
-.proceed:focus{outline:none;box-shadow: inset 0px 0px 5px white;}
-.sendicon{filter:invert(100%); padding-top:2px;}
-
-@media (max-width: 600px){
-  .proceed{transform:translate(250px, 10px);}
-  .col{display:block; margin:auto; width:100%; text-align:center;}
+.form .input-field:hover {
+  border-color: rgba(209, 121, 66, 0.5);
 }
+
+.form .input-field:focus {
+  border-color: #27a776;
+}
+.form .split {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  gap: 15px;
+}
+.form .split label {
+  width: 130px;
+}
+.form .checkout-btn {
+  margin-top: 20px;
+  padding: 20px 0;
+  border-radius: 25px;
+  font-weight: 700;
+  transition: all 0.3s cubic-bezier(0.15, 0.83, 0.66, 1);
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: 500;
+  display: flex;
+  align-items: center;
+  border: none;
+  justify-content: center;
+  fill: #fff;
+  color: #fff;
+  border: 2px solid transparent;
+  background: #27a776;
+  transition: all 200ms;
+}
+.form .checkout-btn:active {
+  scale: 0.95;
+}
+
+.form .checkout-btn:hover {
+  color: #d17842;
+  border: 2px solid #d17842;
+  background: transparent;
+}
+.animate-rotate45 {
+    animation: kfr-rotate45 2s infinite;
+  }
+@keyframes kfr-rotate45 {
+    from {
+      transform: rotate(45deg);
+      opacity: 0;
+    }
+  }
+  .clignote {
+    animation: clignotes 1.5s infinite;
+  }
+
+  @keyframes clignotes {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }}
+
     </style>
 </head>
+
 <body>
-    
-<div class="container">
-    <div class="card1">
-      <button class="proceed" style="display: flex; justify-content: center; align-items: center; width: 50px; height: 50px;"><svg class="sendicon" width="24" height="24" viewBox="0 0 24 24">
-    <path d="M4,11V13H16L10.5,18.5L11.92,19.92L19.84,12L11.92,4.08L10.5,5.5L16,11H4Z"></path>
-  </svg></button>
-     <img src="https://seeklogo.com/images/V/VISA-logo-62D5B26FE1-seeklogo.com.png" class="logo-card">
-   <label>Card number:</label>
-   <input id="user" type="text" class="input cardnumber"  placeholder="1234 5678 9101 1121">
-   <label>Name:</label>
-   <input class="input name"  placeholder="Edgar Pérez">
-   <label class="toleft">CCV:</label>
-   <input class="input toleft ccv" placeholder="321">
-    </div>
-    <div class="receipt">
-      <div class="col"><p>Cost:</p>
-      <h2 class="cost">$400</h2><br>
-      <p>Name:</p>
-      <h2 class="seller">Codedgar</h2>
-      </div>
-      <div class="col">
-        <p>Bought Items:</p>
-        <h3 class="bought-items">Corsair Mouse</h3>
-        <p class="bought-items description">Gaming mouse with shiny lights</p>
-        <p class="bought-items price">$200 (50% discount)</p><br>
-        <h3 class="bought-items">Gaming keyboard</h3>
-        <p class="bought-items description">Look mommmy, it has colors!</p>
-        <p class="bought-items price">$200 (50% discount)</p><br>
-      </div>
-      <p class="comprobe">This information will be sended to your email</p>
-    </div>
-  </div>
+
+    <section class="add-card page">
+        <form class="form " onclick="setperm()">
+          <label for="name" class="label">
+            <span class="title">Card holder full name</span>
+            <input
+              class="input-field"
+              type="text"
+              name="input-name"
+              title="Input title"
+              placeholder="Enter your full name"
+            />
+          </label>
+          <label for="serialCardNumber" class="label">
+            <span class="title">Card Number</span>
+            <input
+              id="serialCardNumber"
+              class="input-field"
+              type="number"
+              name="input-name"
+              title="Input title"
+              placeholder="0000 0000 0000 0000"
+            />
+          </label>
+          <div class="split">
+            <label for="ExDate" class="label">
+              <span class="title">Expiry Date</span>
+              <input
+                id="ExDate"
+                class="input-field"
+                type="text"
+                name="input-name"
+                title="Expiry Date"
+                placeholder="01/23"
+              />
+            </label>
+            <label for="cvv" class="label">
+              <span class="title"> CVV</span>
+              <input
+                id="cvv"
+                class="input-field"
+                type="number"
+                name="cvv"
+                title="CVV"
+                placeholder="CVV"
+              />
+            </label>
+          </div>
+          <input class="checkout-btn" type="button" value="Checkout" />
+        </form>
+      </section>
+      
 </body>
+
 </html>
--- mochtara/resources/views/components/product.blade.php
+++ mochtara/resources/views/components/product.blade.php
@@ -71,6 +71,121 @@
             z-index: 99;
         }
     </style>
+    <style>
+        .button {
+  --width: 100px;
+  --height: 35px;
+  --tooltip-height: 35px;
+  --tooltip-width: 90px;
+  --gap-between-tooltip-to-button: 18px;
+  --button-color: #27a776;
+  --tooltip-color: #fff;
+  width: var(--width);
+  height: var(--height);
+  background: var(--button-color);
+  position: relative;
+  text-align: center;
+  border-radius: 0.45em;
+  font-family: "Arial";
+  transition: background 0.3s;
+}
+
+.button::before {
+  position: absolute;
+  content: attr(data-tooltip);
+  width: var(--tooltip-width);
+  height: var(--tooltip-height);
+  background-color: #555;
+  font-size: 0.9rem;
+  color: #fff;
+  border-radius: .25em;
+  line-height: var(--tooltip-height);
+  bottom: calc(var(--height) + var(--gap-between-tooltip-to-button) + 10px);
+  left: calc(50% - var(--tooltip-width) / 2);
+}
+
+.button::after {
+  position: absolute;
+  content: '';
+  width: 0;
+  height: 0;
+  border: 10px solid transparent;
+  border-top-color: #555;
+  left: calc(50% - 10px);
+  bottom: calc(100% + var(--gap-between-tooltip-to-button) - 10px);
+}
+
+.button::after,.button::before {
+  opacity: 0;
+  visibility: hidden;
+  transition: all 0.5s;
+}
+
+.text {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.button-wrapper,.text,.icon {
+  overflow: hidden;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  color: #fff;
+}
+
+.text {
+  top: 0
+}
+
+.text,.icon {
+  transition: top 0.5s;
+}
+
+.icon {
+  color: #fff;
+  top: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.icon svg {
+  width: 24px;
+  height: 24px;
+}
+
+.button:hover {
+  background: #27a776;
+}
+
+.button:hover .text {
+  top: -100%;
+}
+
+.button:hover .icon {
+  top: 0;
+}
+
+.button:hover:before,.button:hover:after {
+  opacity: 1;
+  visibility: visible;
+}
+
+.button:hover:after {
+  bottom: calc(var(--height) + var(--gap-between-tooltip-to-button) - 20px);
+}
+
+.button:hover:before {
+  bottom: calc(var(--height) + var(--gap-between-tooltip-to-button));
+}
+
+
+
+
+    </style>
 </head>
 
 <body>
@@ -81,13 +196,14 @@
                 d="M891 308H340q-6 0-10.5-4t-5.5-10l-32-164q-2-14-12-22.5T256 99H110q-15 0-25.5 10.5T74 135v5q0 15 10.5 26t25.5 11h102q4 0 7 2.5t4 6.5l102 544q3 19 20 28 8 5 18 5h17q-22 25-21 58.5t25 56.5 57.5 23 58-23 25.5-56.5-22-58.5h186q-23 25-21.5 58.5T693 878t57.5 23 57.5-23 25-56.5-21-58.5h17q15 0 25.5-10.5T865 727v-8q0-15-11-25.5T828 683H409q-6 0-10.5-4t-5.5-9l-10-54q-1-8 4-14t12-5h460q13 0 22.5-8t11.5-21l33-219q3-16-7.5-28.5T891 308z" />
         </svg>
     </button>
+    <style>
+        
+    </style>
     <div id="infoPanel"
-        style="position: fixed; top: 0; right: -100%; width: 100%; height: 100%; background: #000; color: white; transition: right 0.5s; z-index: 99; display: flex; justify-content: center; align-items: center; margin-top :5%;overflow: ;">
-        <button onclick="toggleDiv()" style="border: 2px solid white; color: white; background: #27a776;width:30px;height:30px; border-radius:10px;font-size:25px;display:flex;justify-content: center; align-items: center;">
-            &times;</button>
-            <x-payment />
-
-    </div>
+    onclick="toggleDiv()"
+    style="position: fixed; top: 0; right: -100%; width: 100%; height: 100%; background: #ffffff63; backdrop-filter: 10px; color: white; transition: right 0.5s; z-index: 99; display: flex; justify-content: center; align-items: center; margin-top: 5%; overflow-y: auto;">
+    <x-payment />
+</div>
 
 
     @foreach ($products as $product)
@@ -138,18 +254,23 @@
                             d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
                         </path>
                     </svg>
-                </div>
+                    </div>
                 <div class="flex items-center justify-between">
                     <p class="flex flex-col">
                         <span class="text-base font-bold text-slate-900">{{ $product->price }} MAD</span>
                     </p>
-                    <a href="#hs-vertically-centered-modal-{{ $product->id }}"
-                        class="inline-flex items-center justify-center rounded-md px-5 py-2.5 text-center text-sm font-medium text-black hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300 w-1/2"
-                        style="background: #27a776;" data-hs-overlay="#hs-vertically-centered-modal">
-                        <img src="svg_icon/barcode_code_icon_194645.ico" alt="Shopping" class="mr-2 h-4 w-6">
-                        <span class="text-base">Acheter</span>
-                    </a>
-
+                    <a href="#hs-vertically-centered-modal-{{ $product->id }}" ><div data-tooltip="{{ $product->price }}DH" class="button">
+                        <div class="button-wrapper">
+                          <div class="text">Acheter</div>
+                            <span class="icon">
+                              <svg viewBox="0 0 16 16" class="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
+                        </svg>
+                            </span>
+                          </div>
+                        </div></a>
+                        
+                        
                 </div>
             </div>
         </div>
@@ -255,8 +376,14 @@
     </script>
 
     <script>
+        var perm = true
+        function  setperm(){
+            perm = false
+        }
         function toggleDiv() {
-            var panel = document.getElementById("infoPanel");
+            if(perm){
+               
+                var panel = document.getElementById("infoPanel");
             var button = document.getElementById("toggleButton");
 
             if (panel.style.right === "0px") {
@@ -266,6 +393,10 @@
                 panel.style.right = "0px";
                 button.style.display = "none";
             }
+            }else{
+                perm = true
+            }
+            
         }
     </script>
 
