660393f6c618711319d86f77f8c44f4f79c3cc2c
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-04-24 10:21:35 +0100

    front de payment

--- mochtara/resources/views/components/payment.blade.php
+++ mochtara/resources/views/components/payment.blade.php
@@ -7,6 +7,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <style>
+        .add-card {
+            display: flex;
+            margin: 20px;
+
+        }
+
         .form {
             background: #000000;
             border: 1px solid white;
@@ -139,38 +145,118 @@
                 opacity: 1;
             }
         }
+
+        .container input {
+            position: absolute;
+            opacity: 0;
+            cursor: pointer;
+            height: 0;
+            width: 0;
+        }
+
+        .container {
+            display: block;
+            position: relative;
+            cursor: pointer;
+            font-size: 20px;
+            user-select: none;
+        }
+
+        .checkmark {
+            position: relative;
+            top: 0;
+            left: 0;
+            height: 1.3em;
+            width: 1.3em;
+            border: 2px solid #414141;
+            border-radius: 5px;
+            transition: all 0.4s cubic-bezier(0.23, 1, 0.320, 1);
+        }
+
+        .container input:hover~.checkmark {
+            border: 2px solid #27a776;
+        }
+
+
+        .container input:checked~.checkmark {
+            box-shadow: 0 0 20px #27a776;
+            border: 2px solid #27a776;
+        }
+
+        .checkmark:after {
+            content: "";
+            position: absolute;
+            display: none;
+        }
+
+        .container input:checked~.checkmark:after {
+            display: block;
+        }
+
+        .container .checkmark:after {
+            left: 0.45em;
+            top: 0.25em;
+            width: 0.25em;
+            height: 0.5em;
+            border: solid white;
+            border-width: 0 0.15em 0.15em 0;
+            transform: rotate(45deg);
+        }
     </style>
 </head>
 
 <body>
 
     <section class="add-card page">
-        <form class="form " onclick="setperm()">
-            <label for="name" class="label">
-                <span class="title">Card holder full name</span>
-                <input class="input-field" type="text" name="input-name" title="Input title"
-                    placeholder="Enter your full name" />
-            </label>
-            <label for="serialCardNumber" class="label">
-                <span class="title">Card Number</span>
-                <input id="serialCardNumber" class="input-field" type="number" name="input-name" title="Input title"
-                    placeholder="0000 0000 0000 0000" />
-            </label>
-            <div class="split">
-                <label for="ExDate" class="label">
-                    <span class="title">Expiry Date</span>
-                    <input id="ExDate" class="input-field" type="text" name="input-name" title="Expiry Date"
-                        placeholder="01/23" />
+        <div>
+            <form class="form " onclick="setperm()">
+                <label for="email" class="label">
+                    <span class="title">Your email</span>
+                    <input class="input-field" type="email" name="email" title="Input title"
+                        placeholder="Enter your full name" />
                 </label>
-                <label for="cvv" class="label">
-                    <span class="title"> CVV</span>
-                    <input id="cvv" class="input-field" type="number" name="cvv" title="CVV"
-                        placeholder="CVV" />
+                <label for="name" class="label">
+                    <span class="title">Card holder full name</span>
+                    <input class="input-field" type="text" name="input-name" title="Input title"
+                        placeholder="Enter your full name" />
                 </label>
+                <label for="serialCardNumber" class="label">
+                    <span class="title">Card Number</span>
+                    <input id="serialCardNumber" class="input-field" type="number" name="input-name"
+                        title="Input title" placeholder="0000 0000 0000 0000" />
+                </label>
+                <div class="split">
+                    <label for="ExDate" class="label">
+                        <span class="title">Expiry Date</span>
+                        <input id="ExDate" class="input-field" type="text" name="input-name" title="Expiry Date"
+                            placeholder="01/23" />
+                    </label>
+                    <label for="cvv" class="label">
+                        <span class="title"> CVV</span>
+                        <input id="cvv" class="input-field" type="number" name="cvv" title="CVV"
+                            placeholder="CVV" />
+                    </label>
+                </div>
+                <input class="checkout-btn" type="button" value="Checkout" />
+            </form>
+        </div>
+        <div style="margin-left: 20px;width: 500px; " class="form " onclick="setperm()">
+            <div
+                style="display: flex; align-items: center;border:1px #27a776 solid;border-radius:5px;padding:10px 10px;box-shadow: 0 0 20px #27a776;">
+                <img src="img\9obya.png" style="height: 50px;width:50px;border-radius:10px;margin-right:20px;"
+                    alt="">
+                <div style=" display: flex;flex-direction: column;align-items: center;justify-content: space-between;">
+                    <h5>kghjkllhh</h5>
+                    <p>100MAD</p>
+                </div>
+                <label class="container">
+                    <input checked="checked" type="checkbox">
+                    <div class="checkmark"></div>
+                </label>
+
             </div>
-            <input class="checkout-btn" type="button" value="Checkout" />
-        </form>
     </section>
+
 
 </body>
 
