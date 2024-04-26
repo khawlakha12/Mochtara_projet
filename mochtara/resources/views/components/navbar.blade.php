260f3d73d956b8dea73f868fb9cf42a84a911fba
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-04-26 02:29:47 +0100

    .

--- mochtara/resources/views/components/navbar.blade.php
+++ mochtara/resources/views/components/navbar.blade.php
@@ -14,7 +14,6 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Cardo:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
-
   <!-- Vendor CSS Files -->
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
@@ -27,7 +26,6 @@
 
 <body>
 
-  <!-- ======= Header ======= -->
   <header id="header" class="header d-flex align-items-center fixed-top">
     <div class="container-fluid d-flex align-items-center justify-content-between">
 
@@ -50,7 +48,7 @@
           <li><a href="/ser">Services</a></li>
           <li><a href="/contact" class="">Contact</a></li>
         </ul>
-      </nav><!-- .navbar -->
+      </nav>
 
       <div class="header-social-links">
         <a href="#" class="facebook"><i class="bi bi-whatsapp"></i></a>
